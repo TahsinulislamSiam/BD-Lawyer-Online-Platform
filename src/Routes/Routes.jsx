f93f410790dbefaa626f4a9b4d1c344b6aae3788
Tahsinul islam <mohammadsiam8760@gmail.com>
2025-10-05 20:47:12 +0600

    Project and routes set up

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from '../Root/Root';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+   Component: Root,
+  },
+]);
